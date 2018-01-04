6bd88790e923151ae005047ba93d1b2a65267670
余翼 <yuyi@weidian.com>
2018-01-04 15:38:00 +0800

    [chore] Catalog change

--- template/app.js
+++ template/app.js
@@ -1,10 +1,21 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 
 const app = new Koa()
+const router = new Router()
 
-app.use(async (ctx, next) => {
-  ctx.body = 'hello koa2～～'
+router.get('/', (ctx, next) => {
+  ctx.body = 'hello koa2～～～'
+  console.log(ctx.query)
 })
+
+router.post('/', (ctx, next) => {
+  ctx.body = 'hello koa2 post'
+})
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods())
 
 app.listen(3003)
 console.log('starting at http://localhost:3003')
